d1b7dc277ce4426428b8d1c84ac879d9852f6ebd
Hardik Gayner <hardikgayner@gmail.com>
2025-11-23 11:37:13 +0530

    Initial commit

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import Education from "@/sections/Education";
+import Intro from "@/sections/Intro";
+import NavBar from "@/sections/NavBar";
+import Project from "@/sections/Project";
+import Skills from "@/sections/Skills";
+import Socials from "@/sections/Socials";
+
+export default function Home() {
+  return (
+    <>
+    <NavBar />
+    <Intro />
+    <Education />
+    <Project />
+    <Skills />
+    <Socials />
+    </>
+  );
+}
