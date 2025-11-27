49a19078cdbbe5e0df41ff7e4840fdef34995d86
Hardik Gayner <hardikgayner@gmail.com>
2025-11-27 11:09:46 +0530

    Changed: Added skills to constants

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,61 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import { skills } from "../constants/skills";
 
 export default function Skills() {
-  const skills = [
-    {
-      id: 1,
-      title: "Programming Languages",
-      skills: [
-        { name: "Java", icon: "/icons/java.svg" },
-        { name: "Python", icon: "/icons/python.svg" },
-        { name: "JavaScript", icon: "/icons/js.svg" },
-        { name: "TypeScript", icon: "/icons/ts.svg" },
-        { name: "Go", icon: "/icons/go.svg" },
-        { name: "C++", icon: "/icons/cpp.svg" },
-      ],
-    },
-    {
-      id: 2,
-      title: "Frameworks",
-      skills: [
-        { name: "React", icon: "/icons/react.svg" },
-        { name: "Spring Boot", icon: "/icons/springboot.svg" },
-        { name: "Next.js", icon: "/icons/next.svg" },
-        { name: "Express.js", icon: "/icons/express.svg" },
-        { name: "Node.js", icon: "/icons/node.svg" },
-        { name: "GraphQL", icon: "/icons/graphql.svg" },
-      ],
-    },
-    {
-      id: 3,
-      title: "Databases",
-      skills: [
-        { name: "MySQL", icon: "/icons/mysql.svg" },
-        { name: "PostgreSQL", icon: "/icons/postgresql.svg" },
-        { name: "MongoDB", icon: "/icons/mongodb.svg" },
-        { name: "Redis", icon: "/icons/redis.svg" },
-        { name: "Firebase", icon: "/icons/firebase.svg" },
-        { name: "Supabase", icon: "/icons/supabase.svg" },
-      ],
-    },
-    {
-      id: 4,
-      title: "Dev Tools & Platforms",
-      skills: [
-        { name: "Git", icon: "/icons/git.svg" },
-        { name: "GitHub", icon: "/icons/github.svg" },
-        { name: "figma", icon: "/icons/figma.svg" },
-        { name: "VS Code", icon: "/icons/vscode.svg" },
-        { name: "Docker", icon: "/icons/docker.svg" },
-        { name: "Vercel", icon: "/icons/vercel.svg" },
-        { name: "Render.com", icon: "/icons/render.svg" },
-        { name: "AWS", icon: "/icons/aws.svg" },
-        { name: "Linux", icon: "/icons/linux.svg" },
-        { name: "Postman", icon: "/icons/postman.svg" },
-      ],
-    },
-  ];
 
   return (
     <section className="min-h-screen w-full bg-background items-center justify-center px-4 py-8 overflow-hidden">
